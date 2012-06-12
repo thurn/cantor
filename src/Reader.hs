8379200eefe10e3c94947b0de5f004d754021f43
Derek Thurn <derek@thurn.ca>
2012-06-12 09:41:06 -0700

    implemented identifier and intOrFloat

--- src/Reader.hs
+++ src/Reader.hs
@@ -9,9 +9,13 @@ module Reader (
   Form(..),
   ReadError,
   readString,
+  showForm,
   runTests
 ) where
 
+import Control.Monad.State (State)
+import Text.Parsec hiding (State)
+import Text.Parsec.Indent (runIndent)
 import Test.QuickCheck.All (quickCheckAll)
 
 -- | Forms are the components of the Cantor parse tree. A form can be 1) an
@@ -33,6 +37,55 @@ type ReadError = String
 readString :: String -> Either ReadError [Form]
 readString = undefined
 
+-- | Converts a form into a String representation of it. The resulting string
+-- will consist of legal Cantor syntax and will be able to be parsed by
+-- readString.
+showForm :: Form -> String
+showForm = undefined
+
 -- | Runs quickCheck on all prop_ functions in this module
 runTests :: IO Bool
 runTests = $quickCheckAll
+
+-- | The type for Cantor parsers (indentation-sensitive)
+type IParser a = ParsecT String () (State SourcePos) a
+
+-- | Uses the specified parser to parse a string, returning the result or a
+-- ParseError.
+runParse :: IParser a -> String -> Either ParseError a
+runParse aParser input =
+  runIndent "(unknown source)" $ runParserT aParser () "(unknown_source)" input
+
+-- | Parser which skips zero or more space or tab characters.
+skipSpaces :: IParser String
+skipSpaces = many (oneOf " \t")
+
+-- | Symbols that may legally occur as an identifier as long as they are not the
+-- first character.
+identSymbols :: String
+identSymbols = "!%&*-+=\\|?/<>"
+
+-- | Parses an identifier
+identifier :: IParser Form
+identifier = do
+  c <- letter
+  cs <- many (alphaNum <|> oneOf identSymbols)
+  skipSpaces
+  return $ Ident $ c:cs
+
+-- | Parses the fractional part of a float and returns a Form by appending the
+-- provided string to the result and converting to a Double.
+fraction :: String -> IParser Form
+fraction s = do
+  char '.'
+  digits <- many1 digit
+  return $ Float $ read $ s ++ "." ++ digits
+
+-- | Parses either an Int or a Float, depending on whether a '.' character is
+-- found in the stream.
+intOrFloat :: IParser Form
+intOrFloat = do
+  int <- many1 digit
+  form <- option (Int (read int)) (fraction int)
+  skipSpaces
+  return form
