36f0b16bba58cebb9ffa054ab3e3779af7a49bbe
Derek Thurn <derek@thurn.ca>
2012-06-13 18:08:19 -0700

    moved tests to ReaderTest.hs

--- src/Reader.hs
+++ src/Reader.hs
@@ -1,25 +1,20 @@
 -- This is free and unencumbered software released into the public domain.
 
-{-# LANGUAGE TemplateHaskell #-}
-
 -- | The Reader module is responsible for defining the Form datatype and for
 -- implementing parser functionality for transforming inputs such as strings
 -- into valid parse trees consisting of Forms.
 module Reader (
   Form(..),
   ReadError,
+  InputName,
   readForms,
   showForms,
-  runTests
 ) where
 
-import qualified Control.Monad
 import qualified Control.Monad.State
 import qualified Data.List as List
 import Text.Parsec
 import qualified Text.Parsec.Indent as Parsec.Indent
-import qualified Test.QuickCheck as QuickCheck
-import Test.QuickCheck.All (forAllProperties)
 
 -- | Forms are the components of the Cantor parse tree. A form can be 1) an
 -- atom, such as an identifier or literal, 2) an operator application to other
@@ -41,12 +36,7 @@ type InputName = String
 -- should be a succient description of the origin of this string, e.g., a
 -- filename.
 readForms :: InputName -> String -> Either ReadError [Form]
-readForms inputName string = runParse inputName (many readForm) string
-
-prop_reversible :: [Form] -> Bool
-prop_reversible forms = case readForms "" (showForms forms) of
-  Left err   -> False
-  Right forms' -> forms == forms'
+readForms inputName = runParse inputName (many readForm)
 
 -- | Converts a list of forms into a String representation. The resulting string
 -- will consist of legal Cantor syntax and will be able to be parsed by
@@ -59,26 +49,14 @@ showForm (Int i)   = show i
 showForm (Float f) = show f
 showForm (Ident s) = s
 
-instance QuickCheck.Arbitrary Form where
-  arbitrary = QuickCheck.oneof [
-    Control.Monad.liftM Int QuickCheck.arbitrary,
-    Control.Monad.liftM Float QuickCheck.arbitrary,
-    Control.Monad.liftM Ident arbitraryIdent
-    ]
-
--- | Runs quickCheck on all prop_ functions in this module
-runTests :: IO Bool
-runTests = $forAllProperties $ QuickCheck.quickCheckWithResult args
-  where args = QuickCheck.stdArgs { QuickCheck.maxSuccess = 5000 }
-
 -- | The type for Cantor parsers (indentation-sensitive)
 type IParser a = ParsecT String () (Control.Monad.State.State SourcePos) a
 
 -- | Uses the specified parser to parse a string, returning the result or a
 -- ParseError.
 runParse :: InputName -> IParser a -> String -> Either ParseError a
-runParse source aParser input = Parsec.Indent.runIndent source runParser
-  where runParser = runParserT aParser () source input
+runParse source aParser input = Parsec.Indent.runIndent source run
+  where run = runParserT aParser () source input
 
 -- | Parses a single Form from the input stream.
 readForm :: IParser Form
@@ -98,27 +76,16 @@ identSymbols = "!%&*-+=\\|?/<>"
 -- | Parses an identifier
 identifier :: IParser Form
 identifier = do
-  c <- letter
+  c  <- letter
   cs <- many (alphaNum <|> oneOf identSymbols)
   skipSpaces
   return $ Ident $ c:cs
-
-arbitraryIdent :: QuickCheck.Gen String
-arbitraryIdent = do
-  c <- initialChar
-  cs <- QuickCheck.listOf identChar
-  return $ c:cs
-  where letters     = ['a' .. 'z'] ++ ['A' .. 'Z']
-        initialChar = QuickCheck.elements letters
-        identChar   = QuickCheck.elements (letters ++ "0123456789" ++ identSymbols)
-
-
 
 exponentPart :: IParser String
 exponentPart = do
     char 'e'
     minus <- option "" (string "-")
-    int <- many1 digit
+    int   <- many1 digit
     return $ "e" ++ minus ++ int
 
 -- | Parses the fractional part of a float and returns a Form by appending the
@@ -126,15 +93,15 @@ exponentPart = do
 fraction :: String -> IParser Form
 fraction s = do
   char '.'
-  digits <- many1 digit
-  exponent <- option "" exponentPart
-  return $ Float $ read $ s ++ "." ++ digits ++ exponent
+  digits  <- many1 digit
+  expPart <- option "" exponentPart
+  return $ Float $ read $ s ++ "." ++ digits ++ expPart
 
 -- | Parses either an Int or a Float, depending on whether a '.' character is
 -- found in the stream.
 intOrFloat :: IParser Form
 intOrFloat = do
-  int <- many1 digit
+  int  <- many1 digit
   form <- option (Int (read int)) (fraction int)
   skipSpaces
   return form
@@ -144,5 +111,6 @@ initialHyphen = do
   char '-'
   num <- intOrFloat
   return $ case num of
-    Int i -> Int $ negate i
+    Int i   -> Int $ negate i
     Float f -> Float $ negate f
+    _       -> error "Unexpected return from intOrFloat"
--- src/ReaderTest.hs
+++ src/ReaderTest.hs
@@ -2,18 +2,65 @@
 
 {-# LANGUAGE TemplateHaskell #-}
 
-module ReaderTest where
+module ReaderTest (
+  readerTests
+) where
 
-import Test.Framework.Providers.HUnit (testCase)
-import Test.Framework.Providers.QuickCheck2 (testProperty)
-import Test.Framework.TH (testGroupGenerator)
-import Test.HUnit ((@?=))
+import           Control.Monad (liftM)
+import           Reader
+import           Test.Framework (Test)
+import           Test.Framework.Providers.QuickCheck2 (testProperty)
+import           Test.Framework.TH (testGroupGenerator)
+import qualified Test.QuickCheck as QuickCheck
 
+instance QuickCheck.Arbitrary Form where
+  arbitrary = QuickCheck.oneof [
+    liftM Int QuickCheck.arbitrary,
+    liftM Float QuickCheck.arbitrary,
+    liftM Ident arbitraryIdent
+    ]
+
+identInitialChars :: String
+identInitialChars = ['a' .. 'z'] ++ ['A' .. 'Z']
+
+identSubsequentChars :: String
+identSubsequentChars = identInitialChars ++ "0123456789!%&*-+=\\|?/<>"
+
+-- | Generate a string that's a valid identifier name.
+arbitraryIdent :: QuickCheck.Gen String
+arbitraryIdent = do
+  c  <- QuickCheck.elements identInitialChars
+  cs <- QuickCheck.listOf $ QuickCheck.elements identSubsequentChars
+  return $ c:cs
+        
+-- | Reads forms from the provided string and applies the provided predicate to
+-- the result.
+readAndCheck :: String -> ([Form] -> Bool) -> Bool
+readAndCheck input predicate = case readForms "" input of
+  Left _ -> False
+  Right forms -> predicate forms
+
+-- | 'showForms' and 'readForms' are the opposite of each other.
+prop_reversible :: [Form] -> Bool
+prop_reversible forms = readAndCheck (showForms forms) (== forms)
+
+-- | Any Haskell integer in its standard string representation can be parsed as
+-- an Int form.
+prop_readInt :: Integer -> Bool
+prop_readInt int = readAndCheck (show int) (== [Int int])
+
+-- | Likewise for doubles.
+prop_readFloat :: Double -> Bool
+prop_readFloat float = readAndCheck (show float) (== [Float float])
+
+newtype ArbitraryIdent = ArbitraryIdent String deriving (Show, Read, Eq)
+
+instance QuickCheck.Arbitrary ArbitraryIdent where
+  arbitrary = liftM ArbitraryIdent arbitraryIdent
+
+-- | Generate an arbitrary identifier and try reading it.
+prop_readIdent :: ArbitraryIdent -> Bool
+prop_readIdent (ArbitraryIdent ident) = readAndCheck ident (== [Ident ident])
+
+readerTests :: Test
 readerTests = $testGroupGenerator
-
-case_1 = do 1 @?= 12
-
-case_2 = do 2 @?= 2
-
-prop_reverse xs = reverse (reverse xs) == xs
-    where types = xs::[Int]
