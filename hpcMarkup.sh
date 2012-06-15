e54cf22a22329c49f22c93dce0097c3e4354a8b6
Derek Thurn <derek@thurn.ca>
2012-06-15 09:33:34 -0700

    more thorough tests for vectors and dicts

---
+++ hpcMarkup.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+hpc markup dist/hpc/tix/test-cantor/test-cantor.tix --destdir=dist
--- src/ReaderTest.hs
+++ src/ReaderTest.hs
@@ -31,13 +31,6 @@ identInitialChars = ['a' .. 'z'] ++ ['A' .. 'Z']
 identSubsequentChars :: String
 identSubsequentChars = identInitialChars ++ "0123456789!%&*-+=\\|?/<>"
 
--- | Generate a string that's a valid identifier name.
-arbitraryIdent :: QuickCheck.Gen String
-arbitraryIdent = do
-  c  <- QuickCheck.elements identInitialChars
-  cs <- QuickCheck.listOf $ QuickCheck.elements identSubsequentChars
-  return $ c:cs
-
 arbitraryString :: QuickCheck.Gen String
 arbitraryString = do
   s <- QuickCheck.arbitrary
@@ -64,6 +57,13 @@ readAndCheck input predicate = case readForms "" input of
   Left _ -> False
   Right forms -> predicate forms
 
+-- | Reads forms from the input and checks whether the parse succeeded,
+-- returning (parseSucceeded XNOR shouldSucceed)
+readWithOutcome :: Bool -> String -> Bool
+readWithOutcome shouldSucceed input = case readForms "" input of
+  Left _ -> not shouldSucceed
+  Right _ -> shouldSucceed
+
 -- | 'showForms' and 'readForms' are the opposite of each other.
 prop_reversible :: [Form] -> Bool
 prop_reversible forms = readAndCheck (showForms forms) (== forms)
@@ -77,24 +77,16 @@ prop_readInt int = readAndCheck (show int) (== [Int int])
 prop_readFloat :: Double -> Bool
 prop_readFloat float = readAndCheck (show float) (== [Float float])
 
-newtype ArbitraryIdent = ArbitraryIdent String deriving (Show, Read, Eq)
-
-instance QuickCheck.Arbitrary ArbitraryIdent where
-  arbitrary = liftM ArbitraryIdent arbitraryIdent
-
--- | Generate an arbitrary identifier and try reading it.
-prop_readIdent :: ArbitraryIdent -> Bool
-prop_readIdent (ArbitraryIdent ident) = readAndCheck ident (== [Ident ident])
-
 prop_readString :: String -> Bool
 prop_readString s = readAndCheck (show s) (== [Str s])
 
 -- | Represents strings which are *not* valid Cantor syntax
-newtype InvalidSyntax = InvalidSyntax String deriving (Show, Read, Eq)
+data InvalidSyntax = UnbalancedString String
+                     deriving (Show, Read, Eq)
 
 instance QuickCheck.Arbitrary InvalidSyntax where
   arbitrary = QuickCheck.oneof [
-    liftM InvalidSyntax (balancedString not)
+    liftM UnbalancedString (balancedString not)
     ]
 
 -- | Checks if a string consists of balanced parentheses
@@ -109,25 +101,51 @@ isBalanced list = null $ foldl' op [] list
 -- 'id' as the predicate) or unbalanced parentheses (by passing 'not')
 balancedString :: (Bool -> Bool) -> QuickCheck.Gen String
 balancedString p = QuickCheck.suchThat parens (p . isBalanced)
-  where parens = QuickCheck.listOf $ QuickCheck.elements "()"
+  where parens = QuickCheck.listOf $ QuickCheck.elements "()[]{}"
 
 prop_invalidSyntax :: InvalidSyntax -> Bool
-prop_invalidSyntax (InvalidSyntax s) = case readForms "" s of
-  Left _ -> True
-  Right _ -> False
+prop_invalidSyntax (UnbalancedString s) = readWithOutcome False s
 
--- | Represents strings which should parse as valid Cantor syntax
-newtype ValidSyntax = ValidSyntax String deriving (Show, Read, Eq)
+-- | Generate a string that's a valid identifier name.
+arbitraryIdent :: QuickCheck.Gen String
+arbitraryIdent = do
+  c  <- QuickCheck.elements identInitialChars
+  cs <- QuickCheck.listOf $ QuickCheck.elements identSubsequentChars
+  return $ c:cs
+
+-- | Represents various strings which should parse as valid Cantor syntax
+data ValidSyntax = BalancedString String
+                 | ArbitraryIdent String
+                 | ArbitraryVector String
+                 | ArbitraryDict String
+                   deriving (Show, Read, Eq)
 
 instance QuickCheck.Arbitrary ValidSyntax where
   arbitrary = QuickCheck.oneof [
-    liftM ValidSyntax (balancedString id)
+    liftM BalancedString (balancedString id),
+    liftM ArbitraryIdent arbitraryIdent,
+    liftM ArbitraryVector (arbitraryBetween '[' ']'),
+    liftM ArbitraryDict (arbitraryBetween '{' '}')
     ]
+    
+arbitraryBetween :: Char -> Char -> QuickCheck.Gen String
+arbitraryBetween c1 c2 = QuickCheck.oneof $ map generate [0 .. 4]
+  where generate n = do forms <- QuickCheck.vectorOf n QuickCheck.arbitrary
+                        return ([c1] ++ (showForms forms) ++ [c2])
+
+sexpHeadedBy :: Form -> [Form] -> Bool
+sexpHeadedBy form ((Sexp (head:_)):_) = head == form
+sexpHeadedBy _ _                      = False
 
 prop_validSyntax :: ValidSyntax -> Bool
-prop_validSyntax (ValidSyntax s) = case readForms "" s of
-  Left _ -> False
-  Right _ -> True
+prop_validSyntax (BalancedString s) =
+  readWithOutcome True s
+prop_validSyntax (ArbitraryIdent s) =
+  readAndCheck s (== [Ident s])
+prop_validSyntax (ArbitraryVector s) =
+  readAndCheck s (sexpHeadedBy (Ident "vector"))
+prop_validSyntax (ArbitraryDict s) =
+  readAndCheck s (sexpHeadedBy (Ident "dict"))
 
 readerTests :: Test
 readerTests = $testGroupGenerator
